8a051a32ce61312d803433117877d9eac6a9fefb
elenakalistova <lenka2001kalistova@gmail.com>
2025-02-09 23:29:41 +0200

    add StringProcessor

--- TestTasks/CharCounting/StringProcessor.cs
+++ TestTasks/CharCounting/StringProcessor.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace TestTasks.VowelCounting
+namespace TestTasks.VowelCounting;
+
+public class StringProcessor
 {
-    public class StringProcessor
+    public (char symbol, int count)[] GetCharCount(string veryLongString, char[] countedChars)
     {
-        public (char symbol, int count)[] GetCharCount(string veryLongString, char[] countedChars)
+        if (veryLongString == null || countedChars == null)
+            throw new ArgumentNullException();
+            
+        if (countedChars.Length == 0)
+            throw new ArgumentException("Array is empty.");
+
+        var charCount = new Dictionary<char, int>();
+
+        foreach (var ch in countedChars)
         {
-            throw new NotImplementedException();
+            charCount[char.ToLower(ch)] = 0;
         }
+
+        foreach (var ch in veryLongString.ToLower().Where(ch => charCount.ContainsKey(ch)))
+        {
+            charCount[ch]++;
+        }
+
+        return charCount.Select(kvp => (kvp.Key, kvp.Value)).ToArray();
     }
 }
